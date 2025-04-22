e5d377c283772540d395d8002d2597868647cc2c
Pavlo Yarema <yarema.ketta2002@gmail.com>
2025-04-22 14:46:04 +0300

    add section journey

--- src/components/intro-section/intro-section.tsx
+++ src/components/intro-section/intro-section.tsx
@@ -13,17 +13,17 @@ export function IntroSection({
 }) {
   return (
     <section
-      className={`${className} ${"bg-[var(--secondary-white)] rounded-[45px] p-[40px] h-[550px] flex gap-[50px] justify-between"}`}
+      className={`${className} ${"bg-[var(--secondary-white)] rounded-[45px] items-center p-[40px] flex gap-[50px] justify-between not-md:flex-col"}`}
     >
       <Image
         src={image}
         alt="intro"
         width={530}
         height={520}
-        className="h-[100%] max-w-[45%]"
+        className="h-[100%] max-w-[45%] not-md:max-w-[100%]"
         priority
       ></Image>
-        <div className="flex flex-col gap-10 max-w-[50%]">
+        <div className="flex flex-col gap-10 max-w-[50%] not-md:max-w-[100%]">
             <h1 className="text-center">{title}</h1>
             <p className="text-[20px] overflow-auto">{titleDescription}</p>
         </div>
